55a1e7a17d8069fb7f1cd8d720ba01a4fb7ca7f4
jana2406429 <jana2406429@miuegypt.edu.eg>
2026-06-07 19:05:51 +0300

    Refactor authentication middleware and add admin check

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,22 @@
 // Block access if user is not logged in
 exports.requireLogin = (req, res, next) => {
-    if (!req.session.user) {
-        return res.redirect('/user/login');
-    }
-    next();
+  if (!req.session.user) {
+    return res.redirect('/user/login');
+  }
+  next();
 };
 
 // Redirect logged-in users away from login and signup pages
 exports.redirectIfLoggedIn = (req, res, next) => {
-    if (req.session.user) {
-        return res.redirect('/');
-    }
-    next();
+  if (req.session.user) {
+    return res.redirect('/');
+  }
+  next();
+};
+
+// Block access if user is not an admin
+exports.requireAdmin = (req, res, next) => {
+  if (!req.session.user) return res.redirect('/user/login');
+  if (req.session.role !== 'admin') return res.redirect('/');
+  next();
 };
